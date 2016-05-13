fd149717eac22fb59df3a10d29b90587de51c938
ehsangolshani <ehsanroman74@gmail.com>
2016-05-13 16:35:42 +0430

    change page changed , end session socket handled .

--- presenterHelper/app/api_v1/presentation_events.py
+++ presenterHelper/app/api_v1/presentation_events.py
@@ -7,11 +7,22 @@ from flask_socketio import emit, join_room, leave_room, \
     close_room, rooms, disconnect
 
 
-
 @socketio.on('change page', namespace='/presentation')
-def update_presentation(data):
-    emit('page changed',{'page': data['data']},broadcast=True)
+def change_page(data):
+    print(data['page'])
+    emit('page changed', {'page': data['page']}, broadcast=True)
 
+
+@socketio.on('end session', namespace='/presentation')
+def end_session():
+    try:
+        # do what is needed in server when the session in ending
+        print("session ended")
+        emit('session ended', broadcast=True)
+        return 1
+    except Exception as e:
+        print(e)
+        return 0
 
 
 ##############################################################################
@@ -25,15 +36,16 @@ def test_connect():
 def test_disconnect():
     print('Client disconnected')
 
+
 @socketio.on('my event', namespace='/presentation')
 def test_message(message):
     session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my response',{'data': message['data']})
+    emit('my response', {'data': message['data']})
 
 
 @socketio.on('my broadcast event', namespace='/presentation')
 def test_broadcast_message(message):
-    emit('my response',{'data': message['data']},broadcast=True)
+    emit('my response', {'data': message['data']}, broadcast=True)
 
 
 @socketio.on('join', namespace='/presentation')
@@ -50,16 +62,16 @@ def leave(message):
 
 @socketio.on('close room', namespace='/presentation')
 def close(message):
-    emit('my response', {'data': 'Room ' + message['room'] + ' is closing.'},room=message['room'])
+    emit('my response', {'data': 'Room ' + message['room'] + ' is closing.'}, room=message['room'])
     close_room(message['room'])
 
 
 @socketio.on('my room event', namespace='/presentation')
 def send_room_message(message):
-    emit('my response', {'data': message['data']},room=message['room'])
+    emit('my response', {'data': message['data']}, room=message['room'])
 
 
 @socketio.on('disconnect request', namespace='/presentation')
 def disconnect_request():
-    emit('my response',{'data': 'Disconnected!'})
+    emit('my response', {'data': 'Disconnected!'})
     disconnect()
